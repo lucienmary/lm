8af9a57fb839712a79547d4624e1d077abc9d771
Lucien Mary <marylucien1998@gmail.com>
2020-06-19 08:49:40 +0200

    Départ projet

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Accueil | lucienmary.be</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+    </head>
+    <body>
+        <header>
+            <a href="#">ML</a>
+
+            <a href="#">Menu</a>
+
+            <h1>Lucien Mary</h1>
+            <p><strong>Développeur web</strong></p>
+
+            <a href="#">Scroll</a>
+        </header>
+
+        <section>
+            <header>
+                <h2>À propos de moi...</h2>
+            </header>
+
+            <p>Je m’appelle Lucien Mary, j’ai 21 ans. Je suis étudiant en DWM à la Haute École Albert Jacquard de Namur. Je me suis dirigé vers ces études après avoir fini mes secondaires avec une qualification en infographie. Ce que je préfère dans la création web, c’est la programmation en JavaScript.</p>
+
+            <footer>
+                <a href="#">Me contacter ?</a>
+            </footer>
+        </section>
+
+        <section>
+            <header>
+                <h2>Mes projets</h2>
+            </header>
+
+            <p>Cliquez sur un projet pour l'afficher!</p>
+            <ul>
+                <li>
+                    <img src="" alt="Image du projet">
+                    <h3>Projet 01</h3>
+                    <p>Side-project réalisé fin de l'année académique 2018 – 2019. «It's time to work!» est un jeu HTML5 accompagné par un site de présentation.</p>
+                </li>
+            </ul>
+        </section>
+
+        <footer>
+            <a href="#">Me contacter</a>
+            <p>Lucien Mary - 2020</p>
+        </footer>
+    </body>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</html>
