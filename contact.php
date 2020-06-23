4433850ef8248c419ef02f1b902304fc7ffc5f66
Lucien Mary <marylucien1998@gmail.com>
2020-06-23 23:08:51 +0200

    intégration call to action

--- contact.php
+++ contact.php
@@ -43,7 +43,8 @@
             <a href="contact.php">Contact</a>
             <p>Lucien Mary - 2020</p>
         </footer>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     </body>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </html>
--- index.php
+++ index.php
@@ -92,7 +92,10 @@
                         </li>
                     </ul>
 
-                    <a href="contact.php">Me contacter?</a>
+                    <div class="call-to-action">
+                        <a href="contact.php">Me contacter</a>
+                        <a href="">Mes projets</a>
+                    </div>
                 </div>
             </div>
         </section>
@@ -135,10 +138,9 @@
             <a href="contact.php">Contact</a>
             <p>Lucien Mary - 2020</p>
         </footer>
-    </body>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <script src="assets/scripts/animations.js">
 
-    </script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <script src="assets/scripts/animations.js"></script>
+    </body>
 </html>
